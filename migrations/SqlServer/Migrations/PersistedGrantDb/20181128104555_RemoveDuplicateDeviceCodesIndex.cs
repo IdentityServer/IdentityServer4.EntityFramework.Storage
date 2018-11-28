5ef179f7dc228072400d6e4b605da59fc74885e6
Mark Allison <mark@sourcestud.io>
2018-11-28 12:47:58 +0000

    Generated migration for removal of duplicate DeviceCodes index

---
+++ migrations/SqlServer/Migrations/PersistedGrantDb/20181128104555_RemoveDuplicateDeviceCodesIndex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SqlServer.Migrations.PersistedGrantDb
+{
+    public partial class RemoveDuplicateDeviceCodesIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DeviceCodes_UserCode",
+                table: "DeviceCodes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceCodes_UserCode",
+                table: "DeviceCodes",
+                column: "UserCode",
+                unique: true);
+        }
+    }
+}
